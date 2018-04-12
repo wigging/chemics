0bedce04712b82f871d2d6d192253f8adbb9bf43
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2018-04-11 21:12:29 -0400

    move instead of copy sphinx html

--- move_sphinx_html.sh
+++ move_sphinx_html.sh
@@ -1,7 +1,7 @@
-echo -e "\033[1mMove Sphinx html files to docs folder.\033[0m"
+echo -e "\033[1mMove Sphinx html files to docs/ folder.\033[0m"
 
 if [ ! -d "docs" ]; then
-    echo "create docs folder"
+    echo "create docs/ folder"
     mkdir docs/
 fi
 
@@ -10,17 +10,20 @@ shopt -s nullglob dotglob
 files=(docs/*)
 
 if [ ${#files[@]} -gt 0 ]; then
-    echo "remove previous files in docs folder"
+    echo "remove previous files in docs/"
     rm -R docs/*
 fi
 
 if [ ! -d "sphinx/_build/html" ]; then
-    echo "no files in sphinx/build/html"
-    echo "run Sphinx to generate html"
+    echo "no files in sphinx/_build/html/"
+    echo "need to run Sphinx to generate html"
     exit 1
 fi
 
-echo "copy html files to docs folder"
-cp -R sphinx/_build/html/* docs/
+echo "move html files from sphinx/_build/html/ to docs/"
+mv sphinx/_build/html/* docs/
 
-echo "done"
+echo "add .nojekyll file in docs/"
+touch docs/.nojekyll
+
+echo "Done."
