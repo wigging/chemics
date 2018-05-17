744d8be110c89aa12004c490ab20ba7a282d8048
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2018-05-16 22:48:11 -0400

    use properties subpackage

--- chemics/__init__.py
+++ chemics/__init__.py
@@ -2,20 +2,20 @@
 # flake8: noqa
 
 from .fluidization.bubble import *
+from .fluidization.choking_velocity import *
 from .fluidization.emulsion import *
 from .fluidization.minimum_fluidization_velocity import *
 from .fluidization.residence_time import *
+from .fluidization.terminal_velocity import *
 from .fluidization.transport_disengagement import *
 
+from .kinetics.catalyst import *
 from .kinetics.pyrolysis import *
 
-from .pneumatics.choking_velocity import *
-from .pneumatics.terminal_velocity import *
-
-from .thermodynamics.density import *
-from .thermodynamics.pressure import *
-from .thermodynamics.thermal_conductivity import *
-from .thermodynamics.viscosity import *
+from .properties.density import *
+from .properties.pressure import *
+from .properties.thermal_conductivity import *
+from .properties.viscosity import *
 
 from .utilities.constants import *
 from .utilities.conversions import *
---
+++ chemics/kinetics/catalyst.py
@@ -0,0 +1,3 @@
+"""
+Catalyst kinetic schemes
+"""
