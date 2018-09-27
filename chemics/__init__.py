e4a56b313529717d99bf629a6b40c413e881defe
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2018-09-27 16:25:16 -0400

    remove fluidization module

--- chemics/__init__.py
+++ chemics/__init__.py
@@ -1,13 +1,4 @@
-
 # flake8: noqa
-
-from .fluidization.bubble import *
-from .fluidization.choking_velocity import *
-from .fluidization.emulsion import *
-from .fluidization.minimum_fluidization_velocity import *
-from .fluidization.residence_time import *
-from .fluidization.terminal_velocity import *
-from .fluidization.transport_disengagement import *
 
 from .kinetics.catalyst import *
 from .kinetics.pyrolysis import *
