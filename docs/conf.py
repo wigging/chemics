a2ea2fa2499abfd5bb0d4f4aa8e6ffd7af3d681c
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-04-01 14:06:28 -0400

    docs for devol time

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Gavin Wiggins'
 author = 'Gavin Wiggins'
 
 # The short X.Y version
-version = '19.03'
+version = '19.3'
 # The full version, including alpha/beta/rc tags
-release = '19.03'
+release = '19.3'
 
 
 # -- General configuration ---------------------------------------------------
