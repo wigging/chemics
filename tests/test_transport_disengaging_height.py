69d259eaad85464f669c0e5465f25be3276ef532
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-07-22 10:47:55 -0400

    Add test for tdh_horio function

--- tests/test_transport_disengaging_height.py
+++ tests/test_transport_disengaging_height.py
@@ -9,3 +9,8 @@ from pytest import approx
 def test_tdh_chan():
     tdh = cm.tdh_chan(0.3)
     assert tdh == approx(1.7587, rel=1e-2)
+
+
+def test_tdh_horio():
+    tdh = cm.tdh_horio(0.05, 0.3)
+    assert tdh == approx(0.563, rel=1e-2)
