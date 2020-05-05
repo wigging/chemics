da2a2e4e9d8a7f50ebfd31503d0b7955bd9d59ef
Norvell-git <64608160+Norvell-git@users.noreply.github.com>
2020-05-05 10:12:52 +0100

    Update and rename test_bubble_velocity_free.py to test_bubble_velocity_holland.py

--- tests/test_bubble_velocity_holland.py
+++ tests/test_bubble_velocity_holland.py
@@ -1,5 +1,5 @@
 """
-Tests for bubble_velocity_free
+Tests for bubble_velocity, ubr_holland
 
 Running through tests for each bubble regime
 """
@@ -20,46 +20,41 @@ sig = 72.75 # [dynes/cm]
 mu_l = 1.21 # [cP]
 
 
-def test_ubrf1():
+def test_ubr_holland_1():
     """
     Region 1 test
     """
-    ubf = cm.ubrf(0.00003, rho_l, rho_g, sig, mu_l)
-    print(ubf)
+    ubf = cm.ubr_holland(0.00003, rho_l, rho_g, sig, mu_l)
     assert ubf == approx(0.0004046, rel=1e-2)
 
 
-def test_ubrf2():
+def test_ubr_holland_2():
     """
     Region 2 test
     """
-    ubf = cm.ubrf(0.0008, rho_l, rho_g, sig, mu_l)
-    print(ubf)
+    ubf = cm.ubr_holland(0.0008, rho_l, rho_g, sig, mu_l)
     assert ubf == approx(0.1, rel=1e-2)
 
 
-def test_ubrf3():
+def test_ubr_holland_3():
     """
     Region 3 test
     """
-    ubf = cm.ubrf(0.003, rho_l, rho_g, sig, mu_l)
-    print(ubf)
+    ubf = cm.ubr_holland(0.003, rho_l, rho_g, sig, mu_l)
     assert ubf == approx(0.3, rel=1e-2)
 
 
-def test_ubrf4():
+def test_ubr_holland_4():
     """
     Region 4 test
     """
-    ubf = cm.ubrf(0.01, rho_l, rho_g, sig, mu_l)
-    print(ubf)
+    ubf = cm.ubr_holland(0.01, rho_l, rho_g, sig, mu_l)
     assert ubf == approx(0.22, rel=1e-2)
 
 
-def test_ubrf5():
+def test_ubr_holland_5():
     """
     Region 5 test
     """
-    ubf = cm.ubrf(0.1, rho_l, rho_g, sig, mu_l)
-    print(ubf)
+    ubf = cm.ubr_holland(0.1, rho_l, rho_g, sig, mu_l)
     assert ubf == approx(0.7, rel=1e-2)
