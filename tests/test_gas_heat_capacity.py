457a59f90b1e14228d1ce360ce8257a4e28e4ad6
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-04-11 21:24:35 -0400

    Add tests for gas heat capacity module

---
+++ tests/test_gas_heat_capacity.py
@@ -0,0 +1,21 @@
+"""
+Tests for the gas_heat_capacity module.
+"""
+
+import chemics as cm
+from pytest import approx
+
+
+def test_cp1():
+    cp1 = cm.cp_gas('CBrClF2', 700)
+    assert cp1 == approx(97.4982, rel=1e-2)
+
+
+def test_cp2():
+    cp2 = cm.cp_gas('C5H10O2', 850, CAS='75-98-9')
+    assert cp2 == approx(268.4920, rel=1e-2)
+
+
+def test_cp3():
+    cp3 = cm.cp_gas('NO2', 900)
+    assert cp3 == approx(51.0686, rel=1e-2)
