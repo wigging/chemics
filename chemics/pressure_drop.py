61c1cbc086ba89fca857b5d8a35320c559ab7a37
jAniceto <jose.pedro.aniceto@gmail.com>
2021-04-20 22:30:31 +0100

    Fix reference footnote and missing item in toctree

--- chemics/pressure_drop.py
+++ chemics/pressure_drop.py
@@ -1,7 +1,7 @@
 def pressure_drop_ergun(mu, epsilon, u0, rhof, dp):
     """
     Calculate the pressure drop per unit length across a packed bed for all
-    flow conditions (laminar to turbulent) using the Ergun equation [1].
+    flow conditions (laminar to turbulent) using the Ergun equation.
 
     .. math:: -\\frac{\\Delta P}{L} = 150 \\frac{\\mu (1-\\varepsilon)^2 u_0 }{d_p^2 \\varepsilon^3} + 1.75 \\frac{\\rho_f (1-\\varepsilon) u_0^2 }{d_p \\varepsilon^3}
 
@@ -31,8 +31,8 @@ def pressure_drop_ergun(mu, epsilon, u0, rhof, dp):
 
     References
     ----------
-    .. [1] J.D. Seader, E.J. Henley, D.K. Roper. Separation Process Principles.
-           John Wiley & Sons, Inc., 3rd edition, 2011.
+    J.D. Seader, E.J. Henley, D.K. Roper. Separation Process Principles.
+    John Wiley & Sons, Inc., 3rd edition, 2011.
     """
     pressure_drop = 150*mu*(1-epsilon)**2*u0/(dp**2*epsilon**3) + \
         1.75*rhof*(1-epsilon)*u0**2/(dp*epsilon**3)
