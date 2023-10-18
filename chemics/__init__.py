e1270026bb08782a38aeec068bfcb7fe918c41d5
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2023-10-17 23:02:33 -0400

    List all functions in the __init__.py

--- chemics/__init__.py
+++ chemics/__init__.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-
 from .atm_pressure import patm
 from .atomic_elements import atomic_elements
 
@@ -16,8 +14,8 @@ from .dimensionless_numbers import archimedes
 from .dimensionless_numbers import biot
 from .dimensionless_numbers import peclet
 from .dimensionless_numbers import prandtl
-from .dimensionless_numbers import pyroI
-from .dimensionless_numbers import pyroII
+from .dimensionless_numbers import pyrolysis_one
+from .dimensionless_numbers import pyrolysis_two
 from .dimensionless_numbers import reynolds
 from .dimensionless_numbers import schmidt
 from .dimensionless_numbers import sherwood
@@ -36,3 +34,11 @@ from .ultimate_analysis import Ultimate
 
 from .wood import cp_wood
 from .wood import k_wood
+
+__all__ = [
+    'patm', 'atomic_elements', 'biocomp', 'plot_biocomp', 'ChemicalEquation',
+    'slm_to_lpm', 'massfrac_to_molefrac', 'molefrac_to_massfrac', 'archimedes',
+    'biot', 'peclet', 'prandtl', 'pyrolysis_one', 'pyrolysis_two', 'reynolds',
+    'schmidt', 'sherwood', 'flow_regime', 'Gas', 'GasMixture', 'Liquid', 'mw',
+    'mw_mix', 'Proximate', 'Ultimate', 'cp_wood', 'k_wood'
+]
