d108e593b0480d5f15bc38af357b5723650d551b
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2023-10-24 21:16:40 -0400

    Rename mw function to molecular_weight

--- chemics/molecular_weight.py
+++ chemics/molecular_weight.py
@@ -1,8 +1,5 @@
-import numpy as np
 import re
 from .atomic_elements import atomic_elements
-
-__all__ = ['mw', 'mw_mix']
 
 
 def _find_end(tokens):
@@ -69,7 +66,7 @@ def _parse(tokens, stack):
     return _parse(tokens[1:], stack)
 
 
-def mw(formula):
+def molecular_weight(formula):
     """
     Tokenize a molecular formula to determine total molecular weight.
     Calculation is based on atomic weight values from IUPAC [1]_.
@@ -104,45 +101,3 @@ def mw(formula):
     tokens = re.findall(r'[A-Z][a-z]*|\d+|\(|\)', formula)
     molecular_weight = _parse(tokens, [])
     return molecular_weight
-
-
-def mw_mix(mws, xs):
-    """
-    Molecular weight of a gas mixture calculated as a weighted mean.
-
-    Parameters
-    ----------
-    mws : list, tuple, or array
-        Molecular weight of each gas component in g/mol
-    xs : list, tuple, or array
-        Mole fraction of each gas component
-
-    Returns
-    -------
-    mw_mix : float
-        Molecular weight of a gas mixture in g/mol
-
-    Raises
-    ------
-    ValueError
-        If sum of mole fractions does not equal 1.0
-
-    Examples
-    --------
-    >>> mw_h2 = cm.mw('H2')
-    ... mw_n2 = cm.mw('N2')
-    ... mw_mix([mw_h2, mw_n2], [0.8, 0.2])
-    7.2156
-
-    >>> mw_h2 = cm.mw('H2')
-    ... mw_n2 = cm.mw('N2')
-    ... mw_ch4 = cm.mw('CH4')
-    ... mw_mix([mw_h2, mw_n2, mw_ch4], [0.4, 0.1, 0.5])
-    11.6293
-    """
-    mws = np.asarray(mws)
-    xs = np.asarray(xs)
-    if not np.isclose(xs.sum(), 1.0):
-        raise ValueError('Sum of mole fractions must be 1.0')
-    molwt_mix = np.average(mws, weights=xs)
-    return molwt_mix
