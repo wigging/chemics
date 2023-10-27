2635b802815edc339d63703b41ccd0741aa471d1
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2023-10-26 21:54:16 -0400

    Fix tests

--- tests/test_gas_mixture.py
+++ tests/test_gas_mixture.py
@@ -9,18 +9,31 @@ from pytest import approx
 def test_viscosity1():
     gas1 = cm.Gas('H2', 773)
     gas2 = cm.Gas('N2', 773)
-
     gas_mixture = cm.GasMixture([gas1, gas2], [0.85, 0.15])
     mu_mix = gas_mixture.viscosity()
-
     assert mu_mix == approx(207.37, rel=1e-2)
 
 
 def test_viscosity2():
     gas1 = cm.Gas('H2', 773)
     gas2 = cm.Gas('N2', 773)
-
     gas_mixture = cm.GasMixture([gas1, gas2], [0.85, 0.15])
     mu_mix = gas_mixture.viscosity(method='herning')
-
     assert mu_mix == approx(252.81, rel=1e-2)
+
+
+def test_molecular_weight1():
+    gas1 = cm.Gas('H2', 773)
+    gas2 = cm.Gas('N2', 773)
+    gas_mixture = cm.GasMixture([gas1, gas2], [0.8, 0.2])
+    mw_mixture = gas_mixture.molecular_weight()
+    assert mw_mixture == approx(7.2156, rel=1e-2)
+
+
+def test_molecular_weight2():
+    gas1 = cm.Gas('H2', 773)
+    gas2 = cm.Gas('N2', 773)
+    gas3 = cm.Gas('CH4', 773)
+    gas_mixture = cm.GasMixture([gas1, gas2, gas3], [0.4, 0.1, 0.5])
+    mw_mixture = gas_mixture.molecular_weight()
+    assert mw_mixture == approx(11.6293, rel=1e-2)
--- tests/test_molecular_weight.py
+++ tests/test_molecular_weight.py
@@ -10,30 +10,15 @@ from pytest import approx
 # ----------------------------------------------------------------------------
 
 def test_carbon():
-    mw = cm.mw('C')
+    mw = cm.molecular_weight('C')
     assert mw == 12.011
 
 
 def test_methane():
-    mw = cm.mw('CH4')
+    mw = cm.molecular_weight('CH4')
     assert mw == approx(16.04, rel=1e-2)
 
 
 def test_ammonium_sulfate():
-    mw = cm.mw('(NH4)2SO4')
+    mw = cm.molecular_weight('(NH4)2SO4')
     assert mw == approx(132.13, rel=1e-2)
-
-
-def test_mix_a():
-    mw_h2 = cm.mw('H2')
-    mw_n2 = cm.mw('N2')
-    mw = cm.mw_mix([mw_h2, mw_n2], [0.8, 0.2])
-    assert mw == approx(7.2156, rel=1e-2)
-
-
-def test_mix_b():
-    mw_h2 = cm.mw('H2')
-    mw_n2 = cm.mw('N2')
-    mw_ch4 = cm.mw('CH4')
-    mw = cm.mw_mix([mw_h2, mw_n2, mw_ch4], [0.4, 0.1, 0.5])
-    assert mw == approx(11.6293, rel=1e-2)
